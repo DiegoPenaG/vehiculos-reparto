1501d1bb7a52ac4220a51734b73903330d9c47b8
Cristopher Contreras <cristopher.contreras.informatica@gmail.com>
2026-04-13 21:16:18 -0400

    feat: agregar clase MotocicletasDeReparto

--- src/modelo/MotocicletasDeReparto.java
+++ src/modelo/MotocicletasDeReparto.java
@@ -2,5 +2,30 @@ package modelo;
 
 public class MotocicletasDeReparto extends Vehiculo {
 
+    private boolean tieneCajaTermica;
 
+    public MotocicletasDeReparto(String patente, String marca, String modelo,
+                                 double capacidadCarga, boolean disponible, boolean tieneCajaTermica) {
+        super(patente, marca, modelo, capacidadCarga, disponible);
+        this.tieneCajaTermica = tieneCajaTermica;
+    }
+
+    public boolean isTieneCajaTermica() {
+        return tieneCajaTermica;
+    }
+
+    public void setTieneCajaTermica(boolean tieneCajaTermica) {
+        this.tieneCajaTermica = tieneCajaTermica;
+    }
+
+    @Override
+    public void mostrarDetalle() {
+        System.out.println("Patente: " + getPatente()
+                + " | Tipo: Moto de reparto"
+                + " | Marca: " + getMarca()
+                + " | Modelo: " + getModelo()
+                + " | Capacidad: " + getCapacidadCarga()
+                + " | Disponible: " + (isDisponible() ? "Sí" : "No")
+                + " | Caja térmica: " + (tieneCajaTermica ? "Sí" : "No"));
+    }
 }
